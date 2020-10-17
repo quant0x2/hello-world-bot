3b07f40d6ad973e0d78e8b50a5e66492818cbf68
quant0x2 <vadikvoskr@gmail.com>
2020-10-17 12:04:17 +0300

    Test websocket client

--- cmd/kintaro/main.go
+++ cmd/kintaro/main.go
@@ -1,6 +1,28 @@
 package main
-import "fmt"
+
+import (
+	"encoding/base64"
+	"fmt"
+	"math/rand"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	var arr = make([]byte, 16)
+	rand.Read(arr)
+	var client http.Client
+	b64 := base64.StdEncoding.EncodeToString(arr)
+	req, er := http.NewRequest("GET", "http://localhost:9001/ws", nil)
+	if er != nil {
+		return
+	}
+	req.Header.Add("Upgrade", "websocket")
+	req.Header.Add("Connection", "Upgrade")
+	req.Header.Add("Sec-WebSocket-Key", b64)
+	req.Header.Add("Sec-WebSocket-Version", "13")
+	_, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error")
+	}
+	//fmt.Println(resp.Header["Sec-WebSocket-Accept"])
 }
